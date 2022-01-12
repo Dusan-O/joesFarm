7baeda6cda971a4a0ba5c60be4dc1030214981bc
Dusan-O <dusan.orescanin@gmail.com>
2022-01-12 15:22:19 +0100

    Joe's [barn]

--- joesFarm.playground/Contents.swift
+++ joesFarm.playground/Contents.swift
@@ -1,46 +1,48 @@
-// Le prix du voyage
+// le prix du voyage
 let price = 1499.0
-
-// Nombre de jours pendant lesquels Joe doit économiser
-var numberOfDay = 0
 
 // L'argent de Joe
 var money = 0.0
 
+// Le nombre de jour pendant lesquels Joe doit économiser
+var numberOfDay = 0
+
+// La grande [lait, blé, laine]
+var barn = [0, 0, 0]
+
 while money < price {
-    // Joe nourrit les vaches tous les jours
+    // Jope nourrit les vaches tous les jours
     money -= 4
     
-    if numberOfDay == 1 {
-    // Joe moisonne
-        money += 100 * 0.30
-    } else if numberOfDay == 10 || numberOfDay == 20 {
-        money += 30 * 1
-    } else {
-        money += 30 * 0.50
+    // On calcule la taille de la grande
+    var barnSize = 0
+    for goods in barn {
+        barnSize += goods
     }
     
+    if barnSize >= 500 {
+        // On vends
+        money += Double(barn[0]) * 0.50 // Lait
+        money += Double(barn[1]) * 0.30 // Blé
+        money += Double(barn[2]) * 1 // Laine
+        
+        // On vide la grande
+        barn = [0, 0, 0]
+    } else {
+        // Journée normale
+        if numberOfDay % 30 == 1 {
+            // Joe moissone
+            barn[1] += 100
+        } else if numberOfDay % 30 == 10 || numberOfDay % 30 == 20 {
+            // Joe tond les motuons
+            barn[2] += 30
+        } else {
+            // Joe trait les vaches
+            barn[0] += 30
+        }
+    }
+    // On passe au jour suivant
     numberOfDay += 1
 }
 
-print("Il aura fallu \(numberOfDay) jours a Joe pour économiser \(money)")
-
-var day = 0 // Le 0 c'est donc lundi
-
-while day < 30 {
-    if day % 7 == 5 || day % 7 == 6 {
-        // C'est le 6e ou 7e jour de la semaine
-        print("Super c'est le week-end !")
-    } else {
-        print("Allez au travail !")
-    }
-    day += 1
-}
-
-if numberOfDay % 30 == 1 {
-    // Joe moissonne
-} else if numberOfDay % 30 == 10 || numberOfDay % 30 == 20 {
-    // Joe tonds les moutons
-} else {
-    // Joe vends son lait
-}
+print("Il aura fallu \(numberOfDay) jours a Joe pour économiser \(money) €")
